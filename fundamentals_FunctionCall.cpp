8311c6e98113587df83d0af3d3a3eed5a3b41841
Navaneet Rao Dhage <navaneetraodhage@gmail.com>
2025-02-07 23:01:56 -0600

    Committing a C++ file to compute 4 variable comparison in called function.

---
+++ fundamentals_FunctionCall.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d)
+{
+    //Writing a single statement to perform 4 variable comparison
+    return ((a > b) || (d > b) || (c > b))? (((a > c) || (d > c))? ((a > d) ? a : d) : c) : b;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
